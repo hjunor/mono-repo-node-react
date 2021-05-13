ded20456b1692637263e4334152fdf5b9b152a3b
Heberth Junor <hjunor@gmail.com>
2021-05-13 16:45:36 -0300

    add: path end files uploads, config multer

---
+++ src/migrations/20210514240820-create_users.js
@@ -0,0 +1,69 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV1,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      biographyId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV1,
+        allowNull: false,
+        references: {
+          model: "biographies",
+          key: "id",
+        },
+      },
+      bankinfoId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV1,
+        allowNull: false,
+        references: {
+          model: "bankinfos",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users ");
+  },
+};
